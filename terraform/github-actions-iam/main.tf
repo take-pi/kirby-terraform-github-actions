729585d29bbf39f2e9d02ae81d9b0f583612c88b
take-pi <106457324+take-pi@users.noreply.github.com>
2026-03-08 23:04:05 +0900

    fix: add createComment call to post terraform plan result to PR

--- terraform/github-actions-iam/main.tf
+++ terraform/github-actions-iam/main.tf
@@ -55,3 +55,37 @@ resource "aws_iam_role_policy_attachment" "github_actions_readonly" {
   role       = aws_iam_role.github_actions.name
   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
+
+# tfstate管理のためのS3・DynamoDBアクセスポリシー
+resource "aws_iam_role_policy" "github_actions_tfstate" {
+  name = "github-actions-tfstate-policy"
+  role = aws_iam_role.github_actions.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::kirby-terraform-tfstate",
+          "arn:aws:s3:::kirby-terraform-tfstate/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem"
+        ]
+        Resource = "arn:aws:dynamodb:ap-northeast-1:*:table/terraform-lock"
+      }
+    ]
+  })
+}
